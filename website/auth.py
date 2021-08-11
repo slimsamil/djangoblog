2de535a561737c6d97888a2579a97b30683699a4
slimsamil <samil-gencaslan@gmx.de>
2021-08-11 11:57:20 +0200

    added navbar and views for login and register page

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-auth = Blueprint("views", __name__)
+auth = Blueprint("auth", __name__)
 
 @auth.route("/register")
 def register():
-    return "<h1>Register</h1>"
+    return render_template("register.html")
 
 @auth.route("/login")
 def login():
-    return "<h1>Login</h1>"
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
-    return "<h1>Logout</h1>"
+    return render_template(url_for("views.home"))
--- website/views.py
+++ website/views.py
@@ -1,8 +1,7 @@
-import re
 from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
-@views.route("/home")
+@views.route("/")
 def home():
     return render_template("home.html")
